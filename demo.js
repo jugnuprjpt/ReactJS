bd00498e6af87ade84034240f620796fcd95c0e6
jugnuprjpt <100986544+jugnuprjpt@users.noreply.github.com>
2022-03-05 11:07:15 +0530

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './header'
+
+function LeagueStandings(props) {
+  return (
+      <>
+      <Header/>
+    <div>LeagueStandings LeagueStandings</div>
+    </>
+  );
+}
+
+
+
+export default LeagueStandings;
